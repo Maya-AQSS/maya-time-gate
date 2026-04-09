4cb15f414bfe32f6d6e15c4834c78ffbd63f421e
Santi <santiselva2022@gmail.com>
2026-04-09 19:11:51 +0200

    feat:Reduzco el tiempo de delay

--- app/src/main/java/com/example/mayatimegate/view/ConfirmationView.kt
+++ app/src/main/java/com/example/mayatimegate/view/ConfirmationView.kt
@@ -1,32 +1,25 @@
 package com.example.mayatimegate.view
 
-import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.*
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import com.example.mayatimegate.R
 import kotlinx.coroutines.delay
@@ -35,9 +28,6 @@ import java.time.format.DateTimeFormatter
 import coil.compose.AsyncImage
 import com.example.mayatimegate.model.EmployeeResponse
 import com.example.mayatimegate.viewmodel.EmployeeViewModel
-import com.example.mayatimegate.model.CheckDoubleSigning
-import com.example.mayatimegate.utils.SoundManager
-import kotlin.time.Duration
 
 /**
  * Vista de Éxito: Se muestra tras una identificación correcta.
@@ -48,127 +38,70 @@ import kotlin.time.Duration
 fun ConfirmationView(
     onTimeOver: () -> Unit,
     viewModel: EmployeeViewModel,
-    navController: NavHostController,
-    soundManager: SoundManager
 ) {
-    val timeout=3000L
-    // Observamos el estado del empleado desde el ViewModel
+
     val employee by viewModel.employeeInfo.observeAsState()
-    var handled by remember { mutableStateOf(false) }
-    val currentEmployee = employee
-    LaunchedEffect(employee) {
 
-        val emp = currentEmployee ?: return@LaunchedEffect
-        Log.d("Empleado", "Empleado: $currentEmployee")
 
-        when (currentEmployee.status) {
-
-            "success" -> {
-
-                handled = true
-                if (emp.isDoubleSigned == false) {
-
-                    viewModel.latestSuccessfulSigning = emp.isSigned
-
-                    if (emp.isSigned) {
-                        soundManager.play("success-in")
-                    } else {
-                        soundManager.play("success-out")
-                    }
-
-                } else {
-                    navController.navigate("double_signing_error")
-                }
-
-                delay(timeout)
-
-                onTimeOver()
-                viewModel.resetData()
-            }
-
-            "error" -> {
-
-                handled = true
-
-                if (emp.message == "connection-error") {
-                    navController.navigate("connection_error")
-                } else {
-                    navController.navigate("error")
-                }
-            }
-
-            "loading" -> Unit
-        }
-    }
+    // Temporizador de visualización: 1.5 segundos son ideales para un feedback rápido
     LaunchedEffect(Unit) {
-        handled = false
+       delay(30000)
+        viewModel.resetData()
+        delay(100)
+        onTimeOver()
     }
 
-    // UI de la pantalla
     Scaffold(
         containerColor = Color(0xFFEEECEB)
     ) { innerPadding ->
-        // Si hay datos del empleado mostramos la confirmación
-        if (employee?.status == "success" && employee?.isDoubleSigned == false){
-            ConfirmationCompose( // Compose principal
+        if (employee != null) {
+            ConfirmationCompose(
                 employee = employee!!,
                 modifier = Modifier.padding(innerPadding)
             )
         } else {
-            // Si no hay datos mostramos loading
-            LoadingCompose()
+           //Mientras cargan los datos o no los encuentra
+                LoadingCompose()
         }
     }
 }
+
 /**
  * Maquetación de la tarjeta de confirmación.
  */
 @Composable
-fun ConfirmationCompose(
-    employee: EmployeeResponse,
-    modifier: Modifier,
-) {
-    // Nombre completo del usuario
+fun ConfirmationCompose(employee: EmployeeResponse, modifier: Modifier) {
+
     val userName = "${employee.name} ${employee.surname}"
-
-    // Tarjeta principal
     Card(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(32.dp),
+        modifier = Modifier.fillMaxSize().padding(32.dp),
         colors = CardDefaults.cardColors(containerColor = Color(0xFFFDFDFD)),
-        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
     ) {
-
         Column(
             modifier = Modifier.fillMaxSize().padding(40.dp),
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-
-            // Logo de la empresa
-            Image(
+            Image( //logo del ceed
                 painter = painterResource(R.drawable.logo_ceedcv),
                 contentDescription = "Logo CEEDCV",
                 modifier = Modifier.size(200.dp)
             )
 
             Spacer(modifier = Modifier.height(10.dp))
-
-            // Imagen circular del usuario
+            //imagen circular
             CircleImage(userName, employee.imageUrl)
 
             Spacer(modifier = Modifier.height(40.dp))
 
-            // Texto con información del fichaje
-            InformationalText(userName, employee.isSigned)
+            // Mostramos el texto de éxito con el nombre real
+            InformationalText(userName)
 
-            if(employee.isLate!!){
-                LateAlert()
-            }
+
         }
     }
 }
+
 /**
  * Componente para mostrar la imagen del usuario en formato circular.
  */
@@ -196,8 +129,10 @@ fun CircleImage(userName: String, url: String?) {
             AsyncImage(
                 model = url,
                 contentDescription = "Foto de perfil",
-                placeholder = painterResource(R.drawable.logo_ceedcv), // logo
+                placeholder = painterResource(R.drawable.logo_ceedcv), // Una imagen gris o logo
                 error = painterResource(R.drawable.ic_error), // Una imagen de aviso
+                onLoading = { println("Coil: Cargando...") },
+                onError = { error -> println("Coil error: ${error.result.throwable}") },
                 modifier = Modifier.fillMaxSize(),
                 contentScale = ContentScale.Crop
             )
@@ -222,7 +157,7 @@ fun CircleImage(userName: String, url: String?) {
  * Bloque de texto con el resumen de la operación realizada.
  */
 @Composable
-fun InformationalText(user: String, signed: Boolean) {
+fun InformationalText(user:String) {
     // Captura la hora exacta en el momento de la composición
     val currentTime = remember {
         LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm"))
@@ -251,33 +186,19 @@ fun InformationalText(user: String, signed: Boolean) {
 
         Spacer(modifier = Modifier.size(20.dp))
 
-        // Saludo personalizado
-        if(signed){
-            Text(
-                text = "Bienvenid@, $user",
-                style = MaterialTheme.typography.displaySmall
-            )
-        }else{
-            Text(
-                text = "Adios, $user",
-                style = MaterialTheme.typography.displaySmall
-            )
-        }
+        // Saludo personalizado 
+        Text(
+            text = "Bienvenid@/Adios, $user",
+            style = MaterialTheme.typography.displaySmall
+        )
 
         Spacer(modifier = Modifier.size(42.dp))
 
         // Confirmación visual de la hora registrada
-        if(signed){
-            Text(
-                text = "Hora de entrada: $currentTime",
-                style = MaterialTheme.typography.titleLarge
-            )
-        }else{
-            Text(
-                text = "Hora de salida: $currentTime",
-                style = MaterialTheme.typography.titleLarge
-            )
-        }
+        Text(
+            text = "Hora de entrada/salida: $currentTime",
+            style = MaterialTheme.typography.titleLarge
+        )
     }
 }
 
@@ -293,45 +214,9 @@ fun LoadingCompose(){ //Vista mientras se cargan los datos
             style = MaterialTheme.typography.displayLarge
         )
         Spacer(modifier = Modifier.height(50.dp))
-        CircularProgressIndicator()//Circulo de progreso
-    }
-}
+        CircularProgressIndicator( //Circulo de progreso
 
-@Composable
-fun LateAlert(){
-    Row(
-        modifier = Modifier.padding(12.dp),
-        horizontalArrangement = Arrangement.Center,
-        verticalAlignment = Alignment.CenterVertically
-    ){
-        Image(
-            painter = painterResource(R.drawable.ic_schedule),
-            contentDescription = "Icono vista tarde",
-            modifier = Modifier.size(120.dp),
-            // Si el recurso es un vector, puedes tintarlo dinámicamente
-            colorFilter = androidx.compose.ui.graphics.ColorFilter.tint(Color(0xFFF4D03F))
-        )
-        Spacer(Modifier.height(32.dp))
-        Text(
-            "La ultima sesion ya ha comenzado",
-            style = MaterialTheme.typography.displaySmall,
-            color = Color(0xFFF4D03F)
         )
     }
 }
 
-
-@Preview(showBackground = true, widthDp = 700, heightDp = 1100)
-@Composable
-fun prev(){
-    Scaffold(
-        containerColor = Color(0xFFEEECEB)
-    ) { innerPadding ->
-        // Si hay datos del empleado mostramos la confirmación
-        ConfirmationCompose( // Compose principal
-            employee = EmployeeResponse("success", 1, "Santi", "Selva", null, null, null, null, false, isLate = true),
-            modifier = Modifier.padding(innerPadding)
-        )
-
-    }
-}
