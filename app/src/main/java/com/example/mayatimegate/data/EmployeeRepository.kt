ee1e917345f33456e6bdd127600da758f659ba58
Santi <santiselva2022@gmail.com>
2026-04-14 17:07:39 +0200

    feat: Corrijo un par de cosas con la navegacion e implemento logica para calcular y buscar por la letra del dni

--- app/src/main/java/com/example/mayatimegate/data/EmployeeRepository.kt
+++ app/src/main/java/com/example/mayatimegate/data/EmployeeRepository.kt
@@ -6,7 +6,7 @@ import retrofit2.Call
 class EmployeeRepository { //Repositorio que implementa las demas clases para realizar la busqueda
     private val api = RetrofitClient.odooApi
 
-    //funcion que devuelve la informacion del empleado
+    //funciones que devuelven la informacion del empleado
     fun searchEmployeeByRfid(rfid: String): Call<EmployeeResponse>{ //busca por rfid
         return api.getEmployeeByRfid(rfid)
     }
