cdb44f4f938e55e912f17579f9d774428c6f346e
Santi <santiselva2022@gmail.com>
2026-04-15 13:04:06 +0200

    feat: Implemento navegacion a la vista de configuracion y pequeña mejora visual en la interfaz

--- app/src/main/java/com/example/mayatimegate/view/ManualIdentificationView.kt
+++ app/src/main/java/com/example/mayatimegate/view/ManualIdentificationView.kt
@@ -123,8 +123,11 @@ fun ManualIdentificationCompose(
 
     val focusManager = LocalFocusManager.current
     Card(
-        modifier = modifier.fillMaxSize().padding(32.dp),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(32.dp),
         colors = CardDefaults.cardColors(containerColor = Color(0xFFFDFDFD)),
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
     ) {
         BackRow(onBackClick = onBackClick)
 
