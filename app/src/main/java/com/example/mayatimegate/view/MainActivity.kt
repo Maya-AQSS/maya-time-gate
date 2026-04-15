374bf1d8bca557c51245c476e56a55ce0ffe8f69
Santi <santiselva2022@gmail.com>
2026-04-15 10:27:19 +0200

    feat: Implemento vista para mostrar errores

--- app/src/main/java/com/example/mayatimegate/view/MainActivity.kt
+++ app/src/main/java/com/example/mayatimegate/view/MainActivity.kt
@@ -24,9 +24,6 @@ class MainActivity : ComponentActivity() {
 
     private lateinit var navController: NavHostController
 
-//    private val employeeViewModel: EmployeeViewModel by lazy {
-//        ViewModelProvider(this)[EmployeeViewModel::class.java]
-//    }
     private val employeeViewModel: EmployeeViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -85,6 +82,18 @@ class MainActivity : ComponentActivity() {
                             },
                             navController = navController,
                             viewModel = employeeViewModel
+                        )
+                    }
+
+                    composable("connection_error"){
+                        ConnectionErrorView(
+                            onTimeOver = {
+                                navController.navigate("login") {
+                                    popUpTo("login") { inclusive = true }
+                                    launchSingleTop = true
+                                }
+                            },
+                            navController = navController
                         )
                     }
                 }
