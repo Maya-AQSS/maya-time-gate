42d775637e443892a30e2d7ea681173bf9e3a03f
Santi <santiselva2022@gmail.com>
2026-04-15 10:21:03 +0200

    feat: Corrijo pequeños errores en la navegacion a la ventana de error

--- app/src/main/java/com/example/mayatimegate/view/ConfirmationView.kt
+++ app/src/main/java/com/example/mayatimegate/view/ConfirmationView.kt
@@ -38,22 +38,33 @@ import com.example.mayatimegate.viewmodel.EmployeeViewModel
 fun ConfirmationView(
     onTimeOver: () -> Unit,
     viewModel: EmployeeViewModel,
+    navController: NavHostController
 ) {
 
     val employee by viewModel.employeeInfo.observeAsState()
 
 
     // Temporizador de visualizacion
-    LaunchedEffect(Unit) {
-       delay(1600)
-        viewModel.resetData()
-        onTimeOver()
+    LaunchedEffect(employee) {
+        println("DEBUG: El estado del empleado es: ${employee?.status}")
+        if(employee != null){
+            if(employee?.status == "success"){ //si el empleado existe se muesta
+                delay(2000)
+                viewModel.resetData()
+                onTimeOver()
+            }else if(employee?.status == "error"){ //si no se muestra error
+                navController.navigate("error")
+                viewModel.resetData()
+
+            }
+        }
+
     }
 
     Scaffold(
         containerColor = Color(0xFFEEECEB)
     ) { innerPadding ->
-        if (employee != null) {
+        if (employee?.name != null) { //solo se muestra si el empleado tiene nombre
             ConfirmationCompose(
                 employee = employee!!,
                 modifier = Modifier.padding(innerPadding)
@@ -202,7 +213,7 @@ fun InformationalText(user:String) {
 }
 
 @Composable
-fun LoadingCompose(employee: EmployeeResponse?){ //Vista mientras se cargan los datos
+fun LoadingCompose(){ //Vista mientras se cargan los datos
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
@@ -213,9 +224,7 @@ fun LoadingCompose(employee: EmployeeResponse?){ //Vista mientras se cargan los 
             style = MaterialTheme.typography.displayLarge
         )
         Spacer(modifier = Modifier.height(50.dp))
-        CircularProgressIndicator( //Circulo de progreso
-
-        )
+        CircularProgressIndicator()//Circulo de progreso
     }
 }
 
