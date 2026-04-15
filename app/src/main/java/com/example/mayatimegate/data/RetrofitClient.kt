b0b58aac8eeb27aaff630ce2ce8b28c079d00f9a
Santi <santiselva2022@gmail.com>
2026-04-15 09:13:54 +0200

    feat: Implemento pantalla de error para fichaje por tarjeta y corrijo un par de errores

--- app/src/main/java/com/example/mayatimegate/data/RetrofitClient.kt
+++ app/src/main/java/com/example/mayatimegate/data/RetrofitClient.kt
@@ -9,7 +9,7 @@ object RetrofitClient { // Parte que se conecta a la api
 
     //10.42.0.1 URL por si usas wifi
     //10.0.2.2 URL si usas cable usb
-    private const val URL = "http://10.0.2.2:8069/" //url de la api
+    private const val URL = "http://10.42.0.1:8069/" //url de la api
 
 
     val odooApi: OdooApi by lazy {
