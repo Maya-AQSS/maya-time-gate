834efb96c9f27f39c7f484ecbaf6a4cba135f8a9
Santi <santiselva2022@gmail.com>
2026-04-16 18:02:13 +0200

    feat: Optimizacion de los TextField de configuracion que usan DataStore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,4 +76,5 @@ dependencies {
     implementation("androidx.compose.runtime:runtime-livedata:1.6.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
     implementation("androidx.datastore:datastore-preferences:1.1.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 }
