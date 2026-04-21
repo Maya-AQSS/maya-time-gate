3c3279709429c026efe71b649018b458aac6958f
Santi <santiselva2022@gmail.com>
2026-04-21 09:40:47 +0200

    feat: Implemento guardado de logs de fichaje en la api de odoo

--- app/src/main/java/com/example/mayatimegate/model/OdooModels.kt
+++ app/src/main/java/com/example/mayatimegate/model/OdooModels.kt
@@ -1,8 +1,5 @@
 package com.example.mayatimegate.model
 
-import com.google.gson.annotations.SerializedName
-import java.sql.Time
-
 // Lo que enviamos (Request)
 data class OdooRequest( //Estructura del json
     val jsonrpc: String = "2.0",
@@ -16,24 +13,3 @@ data class AttendanceParams( //contenido del empleado en el json
     val location_id: Int
 )
 
-
-data class CheckDoubleSigning( // respuesta de la api para comprobar si el fichaje es doble
-    val status: String,
-    @SerializedName("id_odoo")
-    val employee_id: Int,
-    @SerializedName("fichaje_doble")
-    val doubleSigning: Boolean
-)
-
-data class LastSession( // respuesta de la api para comprobar si llega tarde a la ultima sesion
-    val status: String,
-    @SerializedName("fichaje_tarde")
-    val isLate :Boolean,
-)
-
-
-data class LastSigning(
-    val status: String,
-    @SerializedName("tipo_fichaje")
-    val type: Boolean
-)
