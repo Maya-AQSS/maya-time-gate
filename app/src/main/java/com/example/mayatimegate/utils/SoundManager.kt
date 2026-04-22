1eb3e4bb35bdb233decdf6477de04e88c08a1c5c
Santi <santiselva2022@gmail.com>
2026-04-22 09:42:40 +0200

    feat: Implemento alertas sonoras al fichar

---
+++ app/src/main/java/com/example/mayatimegate/utils/SoundManager.kt
@@ -0,0 +1,45 @@
+package com.example.mayatimegate.utils
+
+import android.content.Context
+import android.media.AudioAttributes
+import android.media.SoundPool
+import com.example.mayatimegate.R
+
+class SoundManager(context: Context) {
+
+    private val soundPool: SoundPool
+
+    private val soundMap = mutableMapOf<String, Int>()
+
+
+    init {
+        val audioAttributes = AudioAttributes.Builder()
+            .setUsage(AudioAttributes.USAGE_ASSISTANCE_SONIFICATION)
+            .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+            .build()
+
+        soundPool = SoundPool.Builder()
+            .setMaxStreams(5)
+            .setAudioAttributes(audioAttributes)
+            .build()
+
+        soundMap["success-in"] = soundPool.load(context, R.raw.success3, 1)
+        soundMap["success-out"] = soundPool.load(context, R.raw.exit, 1)
+        soundMap["error"] = soundPool.load(context, R.raw.error3prueba, 1)
+
+
+    }
+
+    fun play(name: String) {
+        val soundId = soundMap[name] ?: return
+
+        soundPool.play(
+            soundId,
+            1f,
+            1f,
+            1,
+            0,
+            1f
+        )
+    }
+}
